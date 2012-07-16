8562fd8fa214a1382abac04176167354fb1ca183
Aubrey Goodman <aubrey.goodman@gmail.com>
2012-07-15 22:33:32 -0400

    Initial Commit

---
+++ Haunt/Haunt.h
@@ -0,0 +1,13 @@
+//
+//  Haunt.h
+//  Haunt
+//
+//  Created by Aubrey Goodman on 7/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Haunt : NSObject
+
+@end
---
+++ Haunt/Haunt.m
@@ -0,0 +1,13 @@
+//
+//  Haunt.m
+//  Haunt
+//
+//  Created by Aubrey Goodman on 7/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Haunt.h"
+
+@implementation Haunt
+
+@end
